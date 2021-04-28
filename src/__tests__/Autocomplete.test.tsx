366f4e22cf3b54e4d4fb6fa3c378057f75ea36c8
rachelekelly893 <rachelekelly893@googlemail.com>
2021-04-28 14:18:24 +0100

    created Autocomplete.test.tsx - tests input box renders and functions are called

---
+++ src/__tests__/Autocomplete.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AutocompleteInput from '../components/AutocompleteInput/AutocompleteInput';
+import { fireEvent } from '@testing-library/react';
+
+describe('Login component tests', () => {
+	let container: HTMLDivElement;
+    const mockSetMovie = jest.fn();
+
+
+	beforeEach(() => {
+		container = document.createElement('div');
+		document.body.appendChild(container);
+		ReactDOM.render(
+			<AutocompleteInput stableSetMovie={mockSetMovie} />,
+			container
+		);
+	});
+
+	afterEach(() => {
+		document.body.removeChild(container);
+		container.remove();
+	});
+
+	it('Renders InputBox correctly', () => {
+		const inputs = container.querySelectorAll('input');
+		expect(inputs).toHaveLength(1);      
+	});
+
+    it('allows movie title to be input then runs setMovie()', () => {
+        const inputs = container.querySelectorAll('input');
+        const movieInput = inputs[0];
+        fireEvent.change(movieInput, { target: { value: 'some movie' } });
+        expect(inputs[0].value).toBe('some movie');
+        expect(mockSetMovie.mock.calls.length).toBe(3);
+    });
+
+});
