223f46869e83ed5c408323d0fb66b08e94e570a4
rachelekelly893 <rachelekelly893@googlemail.com>
2021-04-30 09:32:04 +0100

    refined tests

--- src/__tests__/Autocomplete.test.tsx
+++ src/__tests__/Autocomplete.test.tsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom';
 import AutocompleteInput from '../components/AutocompleteInput/AutocompleteInput';
 import { fireEvent } from '@testing-library/react';
 
-describe('Login component tests', () => {
+describe('Autocomplete component tests', () => {
 	let container: HTMLDivElement;
-    const mockSetMovie = jest.fn();
-
+	const mockSetMovie = jest.fn();
 
 	beforeEach(() => {
 		container = document.createElement('div');
 		document.body.appendChild(container);
-		ReactDOM.render(
-			<AutocompleteInput stableSetMovie={mockSetMovie} />,
-			container
-		);
+		ReactDOM.render(<AutocompleteInput stableSetMovie={mockSetMovie} />, container);
 	});
 
 	afterEach(() => {
@@ -24,15 +20,15 @@ describe('Login component tests', () => {
 
 	it('Renders InputBox correctly', () => {
 		const inputs = container.querySelectorAll('input');
-		expect(inputs).toHaveLength(1);      
+		expect(inputs).toHaveLength(1);
 	});
 
-    it('allows movie title to be input then runs setMovie()', () => {
-        const inputs = container.querySelectorAll('input');
-        const movieInput = inputs[0];
-        fireEvent.change(movieInput, { target: { value: 'some movie' } });
-        expect(inputs[0].value).toBe('some movie');
-        expect(mockSetMovie.mock.calls.length).toBe(3);
-    });
-
+	it('allows movie title to be input then runs setMovie()', () => {
+		const inputs = container.querySelectorAll('input');
+		const movieInput = inputs[0];
+		expect(mockSetMovie.mock.calls.length).toBe(1);
+		fireEvent.change(movieInput, { target: { value: 'some movie' } });
+		expect(inputs[0].value).toBe('some movie');
+		expect(mockSetMovie.mock.calls.length).toBe(3);
+	});
 });
--- src/__tests__/MovieCard.test.tsx
+++ src/__tests__/MovieCard.test.tsx
@@ -1,32 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MovieCard from '../components/MovieCard/MovieCard';
+import { fireEvent } from '@testing-library/react';
 
-describe('Login component tests', () => {
+describe('MovieCard component tests', () => {
+	let container: HTMLDivElement;
+	const mockHandleUpvote = jest.fn();
+	const mockHandleDownvote = jest.fn();
 
-    let container: HTMLDivElement
+	beforeEach(() => {
+		container = document.createElement('div');
+		document.body.appendChild(container);
+		ReactDOM.render(
+			<MovieCard
+				movie={'test movie'}
+				user={'test user'}
+				id={'test id'}
+				votes={0}
+				handleUpvote={mockHandleUpvote}
+				handleDownvote={mockHandleDownvote}
+			/>,
+			container
+		);
+	});
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<MovieCard movie={'test movie'} user={'test user'} id={'test id'} votes={0} handleUpvote={()=>{}} handleDownvote={()=>{}}/>, container);
-    })
+	afterEach(() => {
+		document.body.removeChild(container);
+		container.remove();
+	});
 
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    })
+	it('Renders Movie Card correctly', () => {
+		const inputs = container.getElementsByClassName('MovieCard');
+		expect(inputs).toHaveLength(1);
+		const movieTitle = container.getElementsByTagName('H2');
+		expect(movieTitle[0].innerHTML).toBe('test movie');
+		const UserVotesPs = container.getElementsByTagName('P');
+		expect(UserVotesPs[0].innerHTML).toBe('Added by: test user');
+		expect(UserVotesPs[1].innerHTML).toBe('Upvotes: 0');
+		const buttons = container.getElementsByTagName('BUTTON');
+		expect(buttons).toHaveLength(2);
+	});
 
-    it('Renders Movie Card correctly', () => {
-        const inputs = container.getElementsByClassName('MovieCard');
-        expect(inputs).toHaveLength(1);
-        const movieTitle = container.getElementsByTagName('H2');
-        expect(movieTitle[0].innerHTML).toBe('test movie');
-        const UseVotesPs = container.getElementsByTagName('P');
-        expect(UseVotesPs[0].innerHTML).toBe('Added by: test user');
-        expect(UseVotesPs[1].innerHTML).toBe('Upvotes: 0');
-        const buttons = container.getElementsByTagName('BUTTON');
-        expect(buttons).toHaveLength(2);
-    });
+	it('runs handleUpvote on click upvote button', () => {
+		const buttons = container.getElementsByTagName('BUTTON');
+		let upvoteBtn = buttons[0];
+		expect(mockHandleUpvote.mock.calls.length).toBe(0);
+		fireEvent.click(upvoteBtn);
+		expect(mockHandleUpvote.mock.calls.length).toBe(1);
+	});
 
-})
+    it('runs handleDownvote on click upvote button', () => {
+		const buttons = container.getElementsByTagName('BUTTON');
+		let downvoteBtn = buttons[1];
+		expect(mockHandleDownvote.mock.calls.length).toBe(0);
+		fireEvent.click(downvoteBtn);
+		expect(mockHandleDownvote.mock.calls.length).toBe(1);
+	});
+});
